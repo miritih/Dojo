962c8a325c4a7a1a99aec21c07b0dde858d2fa57
Eric Mwenda <ericmwenda5@gmail.com>
2017-11-02 00:07:41 +0300

    [feature] add app tests, add create_room and add_person commands

---
+++ tests/testAddPerson.py
@@ -0,0 +1,5 @@
+import unittest
+from models.person import Person
+
+class TestCreatePerson(unittest.TestCase):
+    pass
---
+++ tests/testAddRoom.py
@@ -0,0 +1,4 @@
+import unittest
+from models.room import Room
+class TestAddRoom(unittest.TestCase):
+    pass
